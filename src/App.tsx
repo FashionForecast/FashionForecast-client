8c7e52f2467aa8da3ce8719e8a6b15573bb9d1e5
presentKey <kim.hyeonjae4@gmail.com>
2024-08-06 01:33:14 +0900

    chore: stylelint 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import './App.css';
+import styles from './App.module.scss';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,22 +10,35 @@ function App() {
     <>
       <div>
         <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
+          <img src={viteLogo} className={styles.logo} alt='Vite logo' />
         </a>
         <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
+          <img
+            src={reactLogo}
+            className={`${styles.logo} ${styles.react}`}
+            alt='React logo'
+          />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div className={styles.card}>
+        <button
+          onClick={() => setCount((count) => count + 1)}
+          aria-activedescendant='sadf'
+          aria-atomic
+        >
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <span className={styles.nesting}>scss nesting</span>
       </div>
-      <p className='read-the-docs'>
+
+      <div className={`${styles.box} ${styles.yellow}`} />
+      <div className={`${styles.box} ${styles.blue}`} />
+
+      <p className={styles['read-the-docs']}>
         Click on the Vite and React logos to learn more
       </p>
     </>
