0a0c01353e1ec3b94e317247ed72c942b02b4a08
presentKey <kim.hyeonjae4@gmail.com>
2024-08-09 03:21:41 +0900

    feat: redux 기본 설정

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import './index.scss';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes.tsx';
 import TanstackQueryProvider from './contexts/TanstackQueryProvider.tsx';
+import ReduxProvider from './contexts/ReduxProvider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <TanstackQueryProvider>
-      <RouterProvider router={router} />
-    </TanstackQueryProvider>
+    <ReduxProvider>
+      <TanstackQueryProvider>
+        <RouterProvider router={router} />
+      </TanstackQueryProvider>
+    </ReduxProvider>
   </React.StrictMode>
 );
