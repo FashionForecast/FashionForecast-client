7e6eb5ef9b94502d32e2cbc21a3389fd7d1f88fc
presentKey <kim.hyeonjae4@gmail.com>
2024-08-09 03:24:37 +0900

    chore: 리덕스 사용 예시

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,29 @@
+import useAppDispatch from '@/hooks/useAppDispatch';
+import useAppSelector from '@/hooks/useAppSelector';
+import { decrement, increment } from '@/redux/slice/EXAMPLE_counterSlice';
+
 export default function Home() {
-  return <div>홈</div>;
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
+  return (
+    <div>
+      홈
+      <div>
+        <button
+          aria-label='Increment value'
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label='Decrement value'
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
 }
