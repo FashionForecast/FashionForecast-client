8ac5ad939d3fc35c673a8923b83ed408c9d2ee51
presentKey <kim.hyeonjae4@gmail.com>
2024-08-10 01:42:58 +0900

    chore: emotion 및 material 컴포넌트 예시

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,8 @@
+import ExampleText from '@/components/EXAMPLE_TEXT';
 import useAppDispatch from '@/hooks/useAppDispatch';
 import useAppSelector from '@/hooks/useAppSelector';
 import { decrement, increment } from '@/redux/slice/EXAMPLE_counterSlice';
+import { Button } from '@mui/material';
 
 export default function Home() {
   const count = useAppSelector((state) => state.counter.value);
@@ -10,19 +12,22 @@ export default function Home() {
     <div>
       홈
       <div>
-        <button
+        <Button
+          variant='contained'
           aria-label='Increment value'
           onClick={() => dispatch(increment())}
         >
           Increment
-        </button>
-        <span>{count}</span>
-        <button
+        </Button>
+        <ExampleText>{count}</ExampleText>
+        <Button
+          variant='contained'
+          color='warning'
           aria-label='Decrement value'
           onClick={() => dispatch(decrement())}
         >
           Decrement
-        </button>
+        </Button>
       </div>
     </div>
   );
