cc1fa532d4d4d40b201e4493b086c309a4faa083
presentKey <kim.hyeonjae4@gmail.com>
2024-08-10 01:01:26 +0900

    chore: scss 관련 설정 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes.tsx';
 import TanstackQueryProvider from './contexts/TanstackQueryProvider.tsx';
 import ReduxProvider from './contexts/ReduxProvider.tsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
