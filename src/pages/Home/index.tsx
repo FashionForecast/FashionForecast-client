53aaeb278fedf5fb55102df669f590014bb6fecc
presentKey <115006670+presentKey@users.noreply.github.com>
2024-08-10 23:58:55 +0900

    라이브러리 설정 (#7)
    * feat: react-router 기본 설정
    
    * chore: tanstack-query-devtools 설치
    
    * feat: tanstack-query 기본 설정
    
    * chore: pnpm-lock 업데이트
    
    * feat: redux 기본 설정
    
    * feat: redux 기본 hooks
    
    * chore: 리덕스 사용 예시
    
    * chore: material 및 emotion 설치
    
    * chore: scss 관련 설정 제거
    
    * docs: 커밋 컨벤션 rename, remove 추가
    
    * remove: App 컴포넌트
    
    * chore: emotion 및 material 컴포넌트 예시
    
    * chore: emotion stylelint 설정
    
    * docs: 커밋 컨벤션 design 추가
    
    * design: normalize, reset css 추가
    
    * chore: html lang ko 변경
    
    * remove: 필요없는 파일 제거
    
    * style: 컴포넌트 함수 표현식으로 변경
    
    * docs: 폴더 구조, 컴포넌트 readme 작성

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,36 @@
+import ExampleText from '@/components/EXAMPLE_TEXT';
+import useAppDispatch from '@/hooks/useAppDispatch';
+import useAppSelector from '@/hooks/useAppSelector';
+import { decrement, increment } from '@/redux/slice/EXAMPLE_counterSlice';
+import { Button } from '@mui/material';
+
+const Home = () => {
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
+  return (
+    <div>
+      홈
+      <div>
+        <Button
+          variant='contained'
+          aria-label='Increment value'
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </Button>
+        <ExampleText>{count}</ExampleText>
+        <Button
+          variant='contained'
+          color='warning'
+          aria-label='Decrement value'
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
