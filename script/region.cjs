b2fa9b4050174f196c2b4e3dda5a616719735bad
presentKey <115006670+presentKey@users.noreply.github.com>
2024-08-18 19:58:19 +0900

    위치별 좌표 파일 생성 스크립트 작성 (#11)
    * chore: xlsx 라이브러리 설치
    
    * feat: region 스크립트 작성
    
    * feat: region.json 파일
    
    * chore: xlsx 파일 gitignore 추가
    
    * chore: region.cjs 주석 추가
    
    * remove: 필요없는 파일 제거
    
    * fix: 위도, 경도 변경

---
+++ script/region.cjs
@@ -0,0 +1,44 @@
+const xlsx = require('xlsx');
+const fs = require('fs');
+
+/**
+ * 파일 생성 명령어: pnpm region
+ *
+ * 위경도 엑셀 파일은 아래 링크의 참고 문서에서 가져왔습니다.
+ * script 폴더에 '위경도.xlsx' 파일을 넣어주세요.
+ * @link https://www.data.go.kr/data/15084084/openapi.do
+ */
+
+const workbook = xlsx.readFile('./script/위경도.xlsx');
+const sheetName = workbook.SheetNames[0];
+const sheet = workbook.Sheets[sheetName];
+
+console.log('엑셀 데이터 변환중...');
+
+const data = xlsx.utils.sheet_to_json(sheet);
+const regions = [];
+
+for (const v of data) {
+  if (!v['2단계'] || v['3단계']) continue;
+
+  const obj = {
+    region: `${v['1단계']} ${v['2단계']}`,
+    nx: v['위도(시)'],
+    ny: v['경도(시)'],
+  };
+
+  regions.push(obj);
+}
+
+console.log('region.json 파일 생성중...');
+
+const json = JSON.stringify(regions, null, 2);
+
+fs.writeFile('./src/assets/region.json', json, 'utf-8', (err) => {
+  if (err) {
+    console.error('파일 생성 중 오류 발생: ', err);
+    return;
+  }
+
+  console.log('region.json 파일이 생성되었습니다.');
+});
