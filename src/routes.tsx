ea57dfd7e90b8b8cb4b52eba91206458f02e5c88
presentKey <kim.hyeonjae4@gmail.com>
2024-08-18 20:57:41 +0900

    feat: /search 라우트 설정

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,18 @@ import { createBrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import RootLayout from './components/layout/RootLayout';
+import Search from './pages/Search';
 
 export const router = createBrowserRouter([
   {
     element: <RootLayout />,
     errorElement: <NotFound />,
-    children: [{ path: '/', index: true, element: <Home /> }],
+    children: [
+      { path: '/', index: true, element: <Home /> },
+      {
+        path: '/search',
+        element: <Search />,
+      },
+    ],
   },
 ]) as ReturnType<typeof createBrowserRouter>;
