fa07391195247cf53eb0f5adebaa70b50a9350ab
deokbin <deokbin@naver.com>
2024-08-20 16:12:31 +0900

    feat: 현재 날짜 기준으로 기상정보 불러오기 (#13)

--- src/service/weather.ts
+++ src/service/weather.ts
@@ -1,5 +1,15 @@
 import { WeatherResponse } from '@/types/weather';
 
+const date = new Date();
+const month = (date.getMonth() + 1).toString().padStart(2, '0');
+const year = date.getFullYear().toString();
+const day = date.getDate().toString().padStart(2, '0');
+const hour = date.getHours().toString().padStart(2, '0');
+const minute = date.getMinutes().toString().padStart(2, '0');
+const second = date.getSeconds().toString().padStart(2, '0');
+
+const now = `${year}-${month}-${day}T${hour}:${minute}:${second}`;
+
 export async function getWeather(): Promise<WeatherResponse> {
   const offset = 1000 * 60 * 60 * 9;
   const KTCnow = new Date(new Date().getTime() + offset);
