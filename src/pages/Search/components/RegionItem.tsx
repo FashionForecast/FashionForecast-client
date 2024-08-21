d0e20b3195f2c3781b43559aa7ef97cc584591b3
presentKey <kim.hyeonjae4@gmail.com>
2024-08-21 21:07:21 +0900

    feat: 검색 목록 아이템 클릭 시, 해당 지역 저장 구현

--- src/pages/Search/components/RegionItem.tsx
+++ src/pages/Search/components/RegionItem.tsx
@@ -1,17 +1,42 @@
 import CheckIcon from '@/components/icon/Check';
 import * as S from './RegionItem.style';
 import { IconButton } from '@mui/material';
+import { MY_REGIONS } from '@/constants/localStorage/key';
 
 type RegionItemProps = {
   region: string;
   keyword: string;
+  nx: number;
+  ny: number;
 };
 
-const RegionItem = ({ region, keyword }: RegionItemProps) => {
+//TODO: alert 제거
+const RegionItem = ({ region, keyword, nx, ny }: RegionItemProps) => {
   const [before, match, after] = splitText(region, keyword);
+  const handleSaveClick = () => {
+    const saved: Omit<RegionItemProps, 'keyword'>[] = JSON.parse(
+      localStorage.getItem(MY_REGIONS) ?? '[]'
+    );
+
+    if (saved.some((item) => item.region === region)) {
+      alert('해당 지역이 이미 저장되어 있습니다.');
+      return;
+    }
+
+    if (saved.length >= 3) {
+      alert('3개 이상 저장할 수 없습니다.');
+      return;
+    }
+
+    localStorage.setItem(
+      MY_REGIONS,
+      JSON.stringify([...saved, { region, nx, ny }])
+    );
+    alert(`${region} 지역을 저장했습니다.`);
+  };
 
   return (
-    <S.Item divider>
+    <S.Item divider onClick={handleSaveClick}>
       <span>
         {before}
         <strong>{match}</strong>
