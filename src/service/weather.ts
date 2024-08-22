0dfea71fc1dcf28d290ab5193a22f70d457ec187
deokbin <deokbin@naver.com>
2024-08-22 18:23:51 +0900

    날씨 호출 기준 현재 시간으로 변경(#19)

--- src/service/weather.ts
+++ src/service/weather.ts
@@ -1,11 +1,23 @@
 import { WeatherResponse } from '@/types/weather';
 
 export async function getWeather(): Promise<WeatherResponse> {
+
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  const hour = date.getHours().toString().padStart(2, '0');
+  const minute = date.getMinutes().toString().padStart(2, '0');
+  const second = date.getSeconds().toString().padStart(2, '0');
+
+  const now = `${year}-${month}-${day}T${hour}:${minute}:${second}`;
+
+
   try {
     const res = await fetch(
       `${
         import.meta.env.VITE_SERVER_URL
-      }/weather/forecast?now=2024-08-17T15:00:00&nx=60&ny=127`
+      }/weather/forecast?now=${now}&nx=60&ny=127`
     );
     const json = await res.json();
 
