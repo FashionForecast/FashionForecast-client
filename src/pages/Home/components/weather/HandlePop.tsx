f3b63f0fbdcccb08f0231a6e670bf0868e0d682f
deokbin <deokbin@naver.com>
2024-08-23 16:26:04 +0900

    강수확률 처리 함수 구현(#13)

--- src/pages/Home/components/weather/HandlePop.tsx
+++ src/pages/Home/components/weather/HandlePop.tsx
@@ -13,37 +13,37 @@ import Pop100 from '@/components/icon/Pop/Pop_100.svg';
 const handlePoP = (nowPop: number) => {
     if (nowPop === 0) {
         return <img src={Pop0}
-                    style={{width:'1rem', height:'1rem'}} alt='0%' />;
+                    style={{width:'24px', height:'24px'}} alt='0%' />;
     } else if (nowPop === 10) {
         return <img src={Pop10} 
-        style={{width:'1rem', height:'1rem'}} alt='10%' />;
+        style={{width:'24px', height:'24px'}} alt='10%' />;
     } else if (nowPop === 20) {
         return <img src={Pop20} 
-        style={{width:'1rem', height:'1rem'}} alt='20%' />;
+        style={{width:'24px', height:'24px'}} alt='20%' />;
     } else if (nowPop === 30) {
         return <img src={Pop30} 
-        style={{width:'1rem', height:'1rem'}} alt='30%' />;
+        style={{width:'24px', height:'24px'}} alt='30%' />;
     } else if (nowPop === 40) {
         return <img src={Pop40} 
-        style={{width:'1rem', height:'1rem'}} alt='40%' />;
+        style={{width:'24px', height:'24px'}} alt='40%' />;
     } else if (nowPop === 50) {
         return <img src={Pop50} 
-        style={{width:'1rem', height:'1rem'}} alt='50%' />;
+        style={{width:'24px', height:'24px'}} alt='50%' />;
     } else if (nowPop === 60) {
         return <img src={Pop60} 
-        style={{width:'1rem', height:'1rem'}} alt='60%' />;
+        style={{width:'24px', height:'24px'}} alt='60%' />;
     } else if (nowPop === 70) {
         return <img src={Pop70} 
-        style={{width:'1rem', height:'1rem'}} alt='70%' />;
+        style={{width:'24px', height:'24px'}} alt='70%' />;
     } else if (nowPop === 80) {
         return <img src={Pop80} 
-        style={{width:'1rem', height:'1rem'}} alt='80%' />;
+        style={{width:'24px', height:'24px'}} alt='80%' />;
     } else if (nowPop === 90) {
         return <img src={Pop90} 
-        style={{width:'1rem', height:'1rem'}} alt='90%' />;
+        style={{width:'24px', height:'24px'}} alt='90%' />;
     } else if (nowPop === 100) {
         return <img src={Pop100} 
-        style={{width:'1rem', height:'1rem'}} alt='100%' />;
+        style={{width:'24px', height:'24px'}} alt='100%' />;
     }
 
 }
