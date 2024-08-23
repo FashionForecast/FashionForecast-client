60e0dccd9f9e0f6f354f11f0ccbc04793c30df23
presentKey <kim.hyeonjae4@gmail.com>
2024-08-24 00:43:21 +0900

    feat: CustomTextField 컴포넌트의 filled 스타일 구현

---
+++ src/components/CustomTextField/index.tsx
@@ -0,0 +1,31 @@
+import { TextField as MuiTextFiled } from '@mui/material';
+import styled from '@emotion/styled';
+
+type MuiTextFiledProps = React.ComponentProps<typeof MuiTextFiled>;
+
+const CustomTextField = ({ ...rest }: MuiTextFiledProps) => {
+  return <TextFiledBase {...rest} />;
+};
+
+export default CustomTextField;
+
+const TextFiledBase = styled(MuiTextFiled)`
+  & .MuiFilledInput-root {
+    background-color: ${({ theme }) => theme.colors.blueGrey.A06};
+
+    &.MuiInputBase-root::before {
+      border-bottom-color: ${({ theme }) => theme.colors.blueGrey.A42};
+    }
+
+    @media (hover: hover) and (pointer: fine) {
+      &:hover {
+        background-color: ${({ theme }) => theme.colors.blueGrey.A09};
+      }
+    }
+  }
+
+  & input {
+    padding: 6px 36px 6px 12px;
+    color: ${({ theme }) => theme.colors.text.primary};
+  }
+`;
