38140482ab9c5d2573a763051843be4b228d4420
deokbin <deokbin@naver.com>
2024-08-23 16:25:59 +0900

    강수량 처리 함수 구현(#13)

--- src/pages/Home/components/weather/HandlePcp.tsx
+++ src/pages/Home/components/weather/HandlePcp.tsx
@@ -2,20 +2,24 @@ import Clear from '@/components/icon/Pcp/PClear.svg';
 import Rain from '@/components/icon/Pcp/PRain.svg';
 import RainDrop from '@/components/icon/Pcp/PRaindrop.svg';
 
-const handlePcP = (nowPcp: string) => {
-    if (!nowPcp) {
-        return null;
-    }
+const HandlePcP = (nowPcp: string) => {
+
+    let PcpText = '';
+    let PcpIcon = null;
+
+
     if (nowPcp === '강수없음') {
-        return <img src={Clear}
-                    style={{width:'1rem', height:'1rem'}} alt='0%' />;
+        PcpText = '0mm';
+        PcpIcon = <img src={Clear} style={{width:'24px', height:'24px'}} alt='0%' />;
     } else if (nowPcp === '1mm 미만' || Number(nowPcp.slice(0,3))< 3) {
-        return <img src={RainDrop} 
-        style={{width:'1rem', height:'1rem'}} alt='10%' />;
+        PcpText = `${nowPcp}mm`;
+        PcpIcon = <img src={RainDrop} style={{width:'24px', height:'24px'}} alt='10%' />;
     } else  {
-        return <img src={Rain} 
-        style={{width:'1rem', height:'1rem'}} alt='20%' />;
+        PcpText = `${nowPcp}mm`;
+        PcpIcon = <img src={Rain} style={{width:'24px', height:'24px'}} alt='20%' />;
     } 
 
+    return { PcpText, PcpIcon };
+
 }
-export default handlePcP;
+export default HandlePcP;
