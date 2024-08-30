b92b6234c976dad873bec71b516de67021105730
presentKey <kim.hyeonjae4@gmail.com>
2024-08-31 00:24:23 +0900

    feat: time 포맷팅 구현

--- src/pages/Home/components/WeatherTimeLine.tsx
+++ src/pages/Home/components/WeatherTimeLine.tsx
@@ -7,7 +7,7 @@ const WeatherTimeLine = ({ forecasts }: WeatherTimeLimeProps) => {
     <ol>
       {forecasts.map((v, i) => (
         <li key={i}>
-          <time>{v.fcstTime}</time>
+          <time>{timeFormatting(v.fcstTime)}</time>
           <div>
             {v.skyStatus} {v.rainType}
           </div>
@@ -20,3 +20,14 @@ const WeatherTimeLine = ({ forecasts }: WeatherTimeLimeProps) => {
 };
 
 export default WeatherTimeLine;
+
+function timeFormatting(fcstTime: string) {
+  let hour = fcstTime.slice(0, 2);
+  const numHour = Number(hour);
+  const AMPM = numHour < 12 ? 'AM' : 'PM';
+
+  if (numHour === 0) hour = '12';
+  else if (numHour >= 13) hour = (numHour - 12).toString().padStart(2, '0');
+
+  return `${hour} ${AMPM}`;
+}
