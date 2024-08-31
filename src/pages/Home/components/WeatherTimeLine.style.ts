a97ea49bfe93144c18304b5c216f696bf41f7cc6
presentKey <115006670+presentKey@users.noreply.github.com>
2024-08-31 14:18:44 +0900

    타임라인 구현 (#46)
    * chore: react-plugin-svgr 설치 및 설정
    
    * feat: WeatherTimeLine 마크업
    
    * feat: time 포맷팅 구현
    
    * chore: 날씨 아이콘 추가
    
    * feat: 날씨 이미지 구현
    
    * feat: 강수확률이 0 초과일 때만 렌더링
    
    * design: 타임라인 스타일 적용
    
    * feat: 시간 포맷팅 문구 수정
    
    * design: 날씨영역 전체 스타일링 적용
    
    * rename: 파일명 변경
    
    * chore: 가상선택자 before로 변경
    
    * chore: 오타 수정

---
+++ src/pages/Home/components/WeatherTimeLine.style.ts
@@ -0,0 +1,63 @@
+import styled from '@emotion/styled';
+
+const List = styled.ol`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  margin-top: 18px;
+`;
+
+const Item = styled.li`
+  position: relative;
+  display: flex;
+  width: 100%;
+  margin-bottom: 16px;
+
+  &::before {
+    position: absolute;
+    top: -18px;
+    left: calc(50% - 1px);
+    display: block;
+    width: 0;
+    height: 12px;
+    content: '';
+    border: 1px solid ${({ theme }) => theme.colors.blueGrey[300]};
+  }
+`;
+
+const Time = styled.time`
+  width: 100%;
+  color: ${({ theme }) => theme.colors.text.secondary};
+  text-align: right;
+`;
+
+const ImageWrap = styled.div`
+  margin: 0 16px;
+`;
+
+const TmpPopWrap = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const Tmp = styled.div`
+  width: 100%;
+  max-width: 46px;
+`;
+
+const Pop = styled.div`
+  display: flex;
+  width: 100%;
+  color: ${({ theme }) => theme.colors.info.main};
+
+  &::before {
+    width: 1px;
+    height: 24px;
+    margin: 0 8px;
+    content: '';
+    border-left: 1px solid rgb(0 0 0 / 12%);
+  }
+`;
+
+export const S = { List, Item, Time, ImageWrap, TmpPopWrap, Tmp, Pop };
