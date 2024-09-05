f44eaafcab8bd85d24d4a9c309822a03f2db9bf8
presentKey <115006670+presentKey@users.noreply.github.com>
2024-09-05 17:53:41 +0900

    A2HS 구현 (#60)
    * feat: manifest 설정
    
    * feat: BeforeInstallPropmptEvent 타입 정의
    
    * feat: a2hs 기본 동작 구현
    
    * feat: 스와이프 기능 구현
    
    * design: A2hsSnackbar 스타일 적용
    
    * feat: A2HS 5초 후 노출 및 로직 수정
    
    * design: UX 개선

---
+++ src/components/layout/RootLayout/components/A2hsSnackbar.tsx
@@ -0,0 +1,76 @@
+import { useEffect, useRef, useState } from 'react';
+import useA2HS from '../hooks/useA2HS';
+import { C } from './A2hsSnackbar.style';
+import { SnackbarProps } from '@mui/material';
+
+const snackbarPosition: SnackbarProps['anchorOrigin'] = {
+  vertical: 'bottom',
+  horizontal: 'center',
+};
+
+const A2hsSnackbar = () => {
+  const { deferredPrompt, installApp, clearPrompt: hideSnackbar } = useA2HS();
+  const [touchStartX, setTouchStartX] = useState(0);
+  const [touchEndX, setTouchEndX] = useState(0);
+  const snackbarRef = useRef<HTMLDivElement>(null);
+
+  const handlePointerDown = (e: React.PointerEvent) => {
+    setTouchStartX(e.clientX);
+  };
+
+  const handlePointerMove = (e: React.PointerEvent) => {
+    if (touchStartX !== 0) {
+      setTouchEndX(e.clientX);
+    }
+  };
+
+  const handlePointerEnd = () => {
+    if (!snackbarRef.current) return;
+
+    const snackbarWidth = snackbarRef.current.offsetWidth;
+    const swipeDistance = Math.abs(touchStartX - touchEndX);
+
+    if (swipeDistance >= snackbarWidth / 2 && touchEndX !== 0) {
+      hideSnackbar();
+    } else {
+      setTouchEndX(0);
+    }
+
+    setTouchStartX(0);
+  };
+
+  useEffect(() => {
+    if (!snackbarRef.current) return;
+
+    const snackbarWidth = snackbarRef.current.offsetWidth;
+    const swipeDistance = Math.abs(touchStartX - touchEndX);
+
+    const opacity = Math.max(1 - swipeDistance / snackbarWidth, 0);
+
+    snackbarRef.current.style.opacity = `${opacity}`;
+    snackbarRef.current.style.transform = `translateX(-50%) translateX(${
+      touchEndX - touchStartX
+    }px)`;
+  }, [touchEndX]);
+
+  return (
+    deferredPrompt && (
+      <C.Snackbar
+        ref={snackbarRef}
+        onPointerDown={handlePointerDown}
+        onPointerMove={handlePointerMove}
+        onPointerUp={handlePointerEnd}
+        open={true}
+        anchorOrigin={snackbarPosition}
+        message='홈화면에 바로가기를 추가할 수 있어요!'
+        action={
+          <C.Button size='small' onClick={installApp}>
+            추가하기
+          </C.Button>
+        }
+      />
+    )
+  );
+};
+
+export default A2hsSnackbar;
