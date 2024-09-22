922d5c7832da40f8dc44f6a645d977b0ee5ad46d
presentKey <kim.hyeonjae4@gmail.com>
2024-09-22 18:35:20 +0900

    fix: 모바일 time selector 버그 수정

--- src/pages/Home/components/TimeCarousel.tsx
+++ src/pages/Home/components/TimeCarousel.tsx
@@ -27,8 +27,8 @@ const TimeCarousel = ({
   const itemsRef = useRef<Array<HTMLElement | null>>([]);
 
   const handleDragging = (e: React.PointerEvent) => {
-    e.preventDefault();
     if (!carouselRef.current || !isDragging) return;
+    e.preventDefault();
 
     const positionDiff = e.pageY - prevPageY;
     carouselRef.current.scrollTop = prevScrollTop - positionDiff;
@@ -41,57 +41,61 @@ const TimeCarousel = ({
 
   const handleDragStart = (e: React.PointerEvent) => {
     if (!carouselRef.current) return;
+    e.preventDefault();
     setIsDragging(true);
     setPrevPageY(e.pageY);
     setPrevScrollTop(carouselRef.current.scrollTop);
   };
 
-  const handleDragStop = () => {
+  const handleDragStop = (e: React.PointerEvent) => {
+    e.preventDefault();
     setIsDragging(false);
     setUserSelected(times[currentIndex]);
     updateSelectedTime(type, times[currentIndex]);
     itemsRef.current[currentIndex]?.scrollIntoView({
-      behavior: 'smooth',
       block: 'center',
     });
   };
 
   useEffect(() => {
-    let index = 0;
-    if (type === 'end') {
-      const selectedIndex = times.indexOf(userSelected);
-      index = selectedIndex >= 0 ? selectedIndex : 0;
-    }
+    const selectedIndex = times.indexOf(userSelected);
+    const index = selectedIndex >= 0 ? selectedIndex : 0;
 
     setCurrentIndex(index);
     updateSelectedTime(type, times[index]);
     itemsRef.current[index]?.scrollIntoView({
-      behavior: 'smooth',
       block: 'center',
     });
-  }, [times]);
+  }, [times, userSelected]);
 
   return (
-    <S.Carousel
-      ref={carouselRef}
-      onPointerMove={handleDragging}
-      onPointerDown={handleDragStart}
-      onPointerUp={handleDragStop}
-      onPointerLeave={handleDragStop}
-    >
-      {times.map((time, index) => (
-        <S.Item
-          key={time}
-          className={`${currentIndex === index && 'is-active'}`}
-          data-index={index}
-          ref={(el) => {
-            itemsRef.current[index] = el;
-          }}
-        >
-          {time}
-        </S.Item>
-      ))}
-    </S.Carousel>
+    <>
+      {/* {
+        <div>
+          {userSelected} {currentIndex}
+        </div>
+      } */}
+      <S.Carousel
+        ref={carouselRef}
+        onPointerMove={handleDragging}
+        onPointerDown={handleDragStart}
+        onPointerUp={handleDragStop}
+        onPointerLeave={handleDragStop}
+      >
+        {times.map((time, index) => (
+          <S.Item
+            key={time}
+            className={`${currentIndex === index && 'is-active'}`}
+            data-index={index}
+            ref={(el) => {
+              itemsRef.current[index] = el;
+            }}
+          >
+            {time}
+          </S.Item>
+        ))}
+      </S.Carousel>
+    </>
   );
 };
 
