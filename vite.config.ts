060e4b75c69fc7ca510339f5e634f375a313b1f4
presentKey <kim.hyeonjae4@gmail.com>
2024-09-23 20:58:39 +0900

    chore: vite plugin basic ssl 디펜던시 제거

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import svgr from 'vite-plugin-svgr';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), basicSsl()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
   },
