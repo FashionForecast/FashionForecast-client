cacf0c8e380254d956870da9e11875f4fd579b21
presentKey <kim.hyeonjae4@gmail.com>
2024-09-24 22:33:43 +0900

    chore: config build 테스트

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,55 @@ import { resolve } from 'path';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+// export default defineConfig({
+//   plugins: [react(), svgr()],
+//   resolve: {
+//     alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
+//   },
+//   server: {
+//     proxy: {
+//       // '/oauth2': {
+//       //   target: 'http://localhost:8080',
+//       // },
+//       '/api': {
+//         target: 'http://localhost:8080',
+//       },
+//     },
+//   },
+// });
+
+// ----------------------------
+const defaultConfig = {
   plugins: [react(), svgr()],
   resolve: {
     alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
   },
+};
+
+export default defineConfig(({ command }) => {
+  if (command === 'build') {
+    // const isDev = mode === 'development';
+
+    return {
+      ...defaultConfig,
+      server: {
+        proxy: {
+          '/api': {
+            target: 'http://3.35.213.93:8080',
+          },
+        },
+      },
+    };
+  } else {
+    return {
+      ...defaultConfig,
+      server: {
+        proxy: {
+          '/api': {
+            target: 'http://localhost:8080',
+          },
+        },
+      },
+    };
+  }
 });
