faf49edf46fa4cd592268ad0e7705351e18f2a00
presentKey <kim.hyeonjae4@gmail.com>
2024-09-24 22:38:05 +0900

    chore: config 복원

--- vite.config.ts
+++ vite.config.ts
@@ -4,55 +4,19 @@ import { resolve } from 'path';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react(), svgr()],
-//   resolve: {
-//     alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
-//   },
-//   server: {
-//     proxy: {
-//       // '/oauth2': {
-//       //   target: 'http://localhost:8080',
-//       // },
-//       '/api': {
-//         target: 'http://localhost:8080',
-//       },
-//     },
-//   },
-// });
-
-// ----------------------------
-const defaultConfig = {
+export default defineConfig({
   plugins: [react(), svgr()],
   resolve: {
     alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
   },
-};
-
-export default defineConfig(({ command }) => {
-  if (command === 'build') {
-    // const isDev = mode === 'development';
-
-    return {
-      ...defaultConfig,
-      server: {
-        proxy: {
-          '/api': {
-            target: 'http://3.35.213.93:8080',
-          },
-        },
+  server: {
+    proxy: {
+      // '/oauth2': {
+      //   target: 'http://localhost:8080',
+      // },
+      '/api': {
+        target: 'http://localhost:8080',
       },
-    };
-  } else {
-    return {
-      ...defaultConfig,
-      server: {
-        proxy: {
-          '/api': {
-            target: 'http://localhost:8080',
-          },
-        },
-      },
-    };
-  }
+    },
+  },
 });
