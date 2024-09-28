60e2075f6a028fa351869acf9bb2f4dcb5adcd76
presentKey <kim.hyeonjae4@gmail.com>
2024-09-28 21:55:06 +0900

    feat: 추천 옷 loading ui 구현

--- src/pages/Home/components/RecommendClothes/index.tsx
+++ src/pages/Home/components/RecommendClothes/index.tsx
@@ -7,6 +7,7 @@ import { Chip, ToggleButtonGroup } from '@mui/material';
 import { useState } from 'react';
 import useAppSelector from '@/hooks/useAppSelector';
 import clothesImage from '@/constants/imageData/clothesImage';
+import RecommendClothesLoading from './loading';
 
 const COOL = 'COOL',
   NORMAL = 'NORMAL',
@@ -25,7 +26,7 @@ type RecommendClothesProps = {
 const RecommendClothes = ({ weather }: RecommendClothesProps) => {
   const [tempCondition, setTempCondition] = useState<TempCondition>(NORMAL);
   const currentRegion = useAppSelector((state) => state.currentRegion.value);
-  const { data, isError } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: ['clothes', tempCondition, currentRegion?.region],
     queryFn: () => getDefaultClothes({ ...weather, tempCondition }),
   });
@@ -41,6 +42,8 @@ const RecommendClothes = ({ weather }: RecommendClothesProps) => {
   if (isError) return <div>추천 옷 오류가 발생했습니다.</div>;
   return (
     <S.Section>
+      {isLoading && <RecommendClothesLoading />}
+
       {data?.data.map(({ names, outfitType }) => (
         <C.ClothesCard elevation={0} key={outfitType} $outfitType={outfitType}>
           <S.ImageWrap>
