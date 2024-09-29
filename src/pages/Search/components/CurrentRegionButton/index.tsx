32be64ea75f4ba17ae5bced3aaed0557821eb242
presentKey <115006670+presentKey@users.noreply.github.com>
2024-09-29 19:05:47 +0900

    geolocation 리팩토링 (#82)
    * remove: 예시 파일 제거
    
    * remove: currentRegion 전역 상태 제거
    
    * feat: geolocation 전역 상태 구현
    
    * refactor: useGeolocation 훅 변경
    
    * refactor: geolocation 전역 상태 및 useGeolocation 적용
    
    * remove: 필요없는 파일 제거

--- src/pages/Search/components/CurrentRegionButton/index.tsx
+++ src/pages/Search/components/CurrentRegionButton/index.tsx
@@ -1,7 +1,4 @@
 import { S } from './style';
-import useAppSelector from '@/hooks/useAppSelector';
-import useAppDispatch from '@/hooks/useAppDispatch';
-import { currentRegionActions } from '@/redux/slice/currentRegionSlice';
 import { useNavigate } from 'react-router-dom';
 import { MY_REGION } from '@/constants/localStorage/key';
 import useGeolocation from '@/hooks/useGeolocation';
@@ -9,34 +6,25 @@ import CustomButton from '@/components/CustomMui/CustomButton';
 import LocationIcon from '@/components/icon/Location';
 
 const CurrentRegionButton = () => {
-  const currentRegion = useAppSelector((state) => state.currentRegion.value);
-  const dispatch = useAppDispatch();
+  const { geolocation, updateGPSRegion } = useGeolocation();
   const navigate = useNavigate();
-  const { geolocation, isProcessing } = useGeolocation();
 
-  //TODO: alert 제거
   const handleClick = () => {
-    if (!isProcessing) {
-      dispatch(currentRegionActions.setCurrentRegion(geolocation));
-      localStorage.removeItem(MY_REGION);
-      navigate('/');
-    }
+    updateGPSRegion();
+    localStorage.removeItem(MY_REGION);
+    navigate('/');
   };
 
   return (
     <S.Wrapper>
       <CustomButton
         variant='outlined'
-        disabled={currentRegion?.isGPS || !geolocation}
+        disabled={geolocation?.isGPS}
         color='inherit'
         size='large'
         fullWidth
         startIcon={
-          <LocationIcon
-            color={
-              currentRegion?.isGPS || !geolocation ? 'disabled' : 'default'
-            }
-          />
+          <LocationIcon color={geolocation?.isGPS ? 'disabled' : 'default'} />
         }
         onClick={handleClick}
       >
