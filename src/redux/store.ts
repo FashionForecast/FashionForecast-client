a3a4d83b44c038d1ee82ea330f187631bc5658f9
presentKey <kim.hyeonjae4@gmail.com>
2024-10-03 21:50:40 +0900

    feat: access token 전역 상태 관리

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import geolocationSlice from './slice/geolocationSlice';
+import authSlice from './slice/authSlice';
 
 const store = configureStore({
-  reducer: { geolocation: geolocationSlice },
+  reducer: { auth: authSlice, geolocation: geolocationSlice },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
