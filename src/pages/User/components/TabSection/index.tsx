ddcdb955996cab021f413b4c0e5324a96e099fcb
presentKey <kim.hyeonjae4@gmail.com>
2024-10-06 00:12:05 +0900

    feat: CustomTabs, CustomTab 컴포넌트 구현

--- src/pages/User/components/TabSection/index.tsx
+++ src/pages/User/components/TabSection/index.tsx
@@ -1,7 +1,10 @@
-import { Tab, Tabs } from '@mui/material';
 import { useState } from 'react';
 import Lookbook from './Lookbook';
 import MySetting from './MySetting';
+import CustomTabs from '@/components/CustomMui/CustomTabs';
+import CustomTab from '@/components/CustomMui/CustomTab';
+import LookbookIcon from '@/assets/svg/lookbook.svg?react';
+import SettingIcon from '@/assets/svg/setting.svg?react';
 
 type TabState = '룩북' | '내 설정';
 
@@ -14,10 +17,20 @@ const TabSection = () => {
 
   return (
     <section>
-      <Tabs value={tab} onChange={handleTabChange} variant='fullWidth'>
-        <Tab value='룩북' label='룩북' />
-        <Tab value='내 설정' label='내 설정' />
-      </Tabs>
+      <CustomTabs value={tab} onChange={handleTabChange} variant='fullWidth'>
+        <CustomTab
+          value='룩북'
+          label='룩북'
+          icon={<LookbookIcon />}
+          iconPosition='start'
+        />
+        <CustomTab
+          value='내 설정'
+          label='내 설정'
+          icon={<SettingIcon />}
+          iconPosition='start'
+        />
+      </CustomTabs>
 
       <div>
         {tab === '룩북' && <Lookbook />}
