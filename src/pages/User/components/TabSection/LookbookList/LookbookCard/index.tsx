9fa174686810dbbf360138e9b8331f3a62aa77db
presentKey <kim.hyeonjae4@gmail.com>
2024-10-07 21:17:26 +0900

    design: LookbookCard 스타일 적용

--- src/pages/User/components/TabSection/LookbookList/LookbookCard/index.tsx
+++ src/pages/User/components/TabSection/LookbookList/LookbookCard/index.tsx
@@ -1,39 +1,41 @@
 import Add from '@/assets/svg/add.svg?react';
-import { IconButton } from '@mui/material';
+import { S, C } from './style';
 import 민소매 from '@/assets/svg/clothes/민소매.svg?react';
-
 import 후드티 from '@/assets/svg/clothes/후드티.svg?react';
 import 반바지 from '@/assets/svg/clothes/반바지.svg?react';
-
 import 청바지 from '@/assets/svg/clothes/청바지.svg?react';
 
+export type WeatherType = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8';
+
 type LookbookCardProps = {
-  type: '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8';
+  type: WeatherType;
   title: string;
 };
 
 const LookbookCard = ({ type, title }: LookbookCardProps) => {
   return (
-    <li>
-      <div>
+    <S.LookbookCardWrap $color={type}>
+      <S.CardHeader>
         <span>계절 {type}</span>
         <h3>{title}</h3>
-        <IconButton>
-          <Add />
-        </IconButton>
-      </div>
+        <div>
+          <C.IconBtn>
+            <Add />
+          </C.IconBtn>
+        </div>
+      </S.CardHeader>
 
-      <ol>
-        <li>
+      <S.ClothesList>
+        <S.ClothesItem>
           <후드티 />
           <청바지 />
-        </li>
-        <li>
+        </S.ClothesItem>
+        <S.ClothesItem>
           <민소매 />
           <반바지 />
-        </li>
-      </ol>
-    </li>
+        </S.ClothesItem>
+      </S.ClothesList>
+    </S.LookbookCardWrap>
   );
 };
 
--- src/pages/User/components/TabSection/LookbookList/index.tsx
+++ src/pages/User/components/TabSection/LookbookList/index.tsx
@@ -3,7 +3,7 @@ import LookbookCard from './LookbookCard';
 const LookbookList = () => {
   return (
     <ol>
-      <LookbookCard type={'1'} title={'27°C 이상인 날이에요'} />
+      <LookbookCard type={'1'} title={'28°C 이상인 날이에요'} />
       <LookbookCard type={'2'} title={'23°C-28°C 가량의 날이에요'} />
       <LookbookCard type={'3'} title={'20°C-23°C 가량의 날이에요'} />
       <LookbookCard type={'4'} title={'17°C-20°C 가량의 날이에요'} />
