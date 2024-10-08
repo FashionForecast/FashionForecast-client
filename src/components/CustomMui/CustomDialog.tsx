24a6d7b5c6a4634ed69d117b935afe28209f9584
presentKey <kim.hyeonjae4@gmail.com>
2024-10-09 01:36:38 +0900

    feat: CustomDialog 컴포넌트 구현

---
+++ src/components/CustomMui/CustomDialog.tsx
@@ -0,0 +1,24 @@
+import { Dialog as MuiDialog } from '@mui/material';
+import styled from '@emotion/styled';
+
+type MuiDialogProps = React.ComponentProps<typeof MuiDialog>;
+
+const CustomDialog = ({ children, ...rest }: MuiDialogProps) => {
+  return <DialogBase {...rest}>{children}</DialogBase>;
+};
+
+export default CustomDialog;
+
+const DialogBase = styled(MuiDialog)`
+  & .MuiPaper-root {
+    border-radius: 16px;
+  }
+
+  & .MuiDialogTitle-root {
+    ${({ theme }) => theme.typo.h6}
+  }
+
+  & .MuiDialogActions-root {
+    padding: 16px;
+  }
+`;
