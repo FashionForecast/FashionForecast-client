5d8072480e10f152c30c6977df97ff0619aca302
presentKey <kim.hyeonjae4@gmail.com>
2024-10-09 01:37:58 +0900

    feat: 시간 및 두께 설정 클릭 시, Dialog toggle 구현

--- src/pages/User/components/TabSection/MySetting/ThicknessSetMenu/index.tsx
+++ src/pages/User/components/TabSection/MySetting/ThicknessSetMenu/index.tsx
@@ -1,9 +1,77 @@
+import {
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  FormControl,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+} from '@mui/material';
 import MenuItem from '../MenuItem';
 import TshirtIcon from '@/assets/svg/tshirt.svg?react';
+import { useState } from 'react';
+import CustomButton from '@/components/CustomMui/CustomButton';
+import CustomDialog from '@/components/CustomMui/CustomDialog';
 
 const ThicknessSetMenu = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <MenuItem title='기본 옷차림 두께' value='적당하게' icon={<TshirtIcon />} />
+    <>
+      <MenuItem
+        title='기본 옷차림 두께'
+        value='적당하게'
+        icon={<TshirtIcon />}
+        handleClick={handleClickOpen}
+      />
+
+      <CustomDialog fullWidth onClose={handleClose} open={open}>
+        <DialogTitle>기본 옷차림 두께</DialogTitle>
+        <DialogContent>
+          <FormControl>
+            <RadioGroup
+              aria-labelledby='demo-radio-buttons-group-label'
+              defaultValue='female'
+              name='radio-buttons-group'
+            >
+              <FormControlLabel
+                value='시원하게'
+                control={<Radio />}
+                label='시원하게'
+              />
+              <FormControlLabel
+                value='적당하게'
+                control={<Radio />}
+                label='적당하게'
+              />
+              <FormControlLabel
+                value='따뜻하게'
+                control={<Radio />}
+                label='따뜻하게'
+              />
+            </RadioGroup>
+          </FormControl>
+        </DialogContent>
+        <DialogActions>
+          <CustomButton
+            color='inherit'
+            variant='outlined'
+            onClick={handleClose}
+          >
+            취소
+          </CustomButton>
+          <CustomButton>저장</CustomButton>
+        </DialogActions>
+      </CustomDialog>
+    </>
   );
 };
 
--- src/pages/User/components/TabSection/MySetting/TimeSetMenu/index.tsx
+++ src/pages/User/components/TabSection/MySetting/TimeSetMenu/index.tsx
@@ -1,13 +1,72 @@
+import {
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  FormControl,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+} from '@mui/material';
 import MenuItem from '../MenuItem';
 import ClockIcon from '@/assets/svg/clock.svg?react';
+import CustomButton from '@/components/CustomMui/CustomButton';
+import { useState } from 'react';
+import CustomDialog from '@/components/CustomMui/CustomDialog';
 
 const TimeSetMenu = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <MenuItem
-      title='기본 외출시간'
-      value='오늘 오전 08시 - 오후 07시'
-      icon={<ClockIcon />}
-    />
+    <>
+      <MenuItem
+        title='기본 외출시간'
+        value='오늘 오전 08시 - 오후 07시'
+        icon={<ClockIcon />}
+        handleClick={handleClickOpen}
+      />
+
+      <CustomDialog fullWidth onClose={handleClose} open={open}>
+        <DialogTitle>기본 외출시간</DialogTitle>
+        <DialogContent>
+          <FormControl>
+            <RadioGroup
+              aria-labelledby='demo-radio-buttons-group-label'
+              defaultValue='female'
+              name='radio-buttons-group'
+            >
+              <FormControlLabel
+                value='현재 시간으로부터 8시간 동안'
+                control={<Radio />}
+                label='현재 시간으로부터 8시간 동안'
+              />
+              <FormControlLabel
+                value='직접 설정'
+                control={<Radio />}
+                label='직접 설정'
+              />
+            </RadioGroup>
+          </FormControl>
+        </DialogContent>
+        <DialogActions>
+          <CustomButton
+            color='inherit'
+            variant='outlined'
+            onClick={handleClose}
+          >
+            취소
+          </CustomButton>
+          <CustomButton>저장</CustomButton>
+        </DialogActions>
+      </CustomDialog>
+    </>
   );
 };
 
