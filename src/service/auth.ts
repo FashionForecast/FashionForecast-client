734d6bce4484fedb8ca3ea8f157fd717038c4a87
presentKey <kim.hyeonjae4@gmail.com>
2024-10-12 17:39:54 +0900

    feat: getUser 서비스 구현

--- src/service/auth.ts
+++ src/service/auth.ts
@@ -21,3 +21,22 @@ export async function getAccessToken() {
     throw new Error(error as string);
   }
 }
+
+export async function getUser(token: string) {
+  try {
+    const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/member`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const json = await res.json();
+
+    if (!res.ok) {
+      throw new Error(`${json.code}: ${json.message}`);
+    }
+
+    return json.data;
+  } catch (error) {
+    throw new Error(error as string);
+  }
+}
