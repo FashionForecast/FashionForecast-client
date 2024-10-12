7759a6daa4ab9d509b3d66d9453827d3270f81eb
presentKey <kim.hyeonjae4@gmail.com>
2024-10-12 18:21:01 +0900

    feat: setUserGender 서비스 구현

--- src/service/auth.ts
+++ src/service/auth.ts
@@ -40,3 +40,28 @@ export async function getUser(token: string) {
     throw new Error(error as string);
   }
 }
+
+export async function setUserGender(gender: string, accessToken: string) {
+  try {
+    const res = await fetch(
+      `${import.meta.env.VITE_API_BASE_URL}/member/gender`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ gender }),
+      }
+    );
+    const json = await res.json();
+
+    if (!res.ok) {
+      throw new Error(`${json.code}: ${json.message}`);
+    }
+
+    return json;
+  } catch (error) {
+    throw new Error(error as string);
+  }
+}
