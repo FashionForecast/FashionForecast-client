d54820206471c1589fc257bbd958e9734acbda4b
presentKey <kim.hyeonjae4@gmail.com>
2024-10-12 17:41:32 +0900

    feat: storeUser 유틸 함수 구현

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,15 +1,32 @@
 import { LOGIN } from '@/constants/localStorage/key';
 import { authActions } from '@/redux/slice/authSlice';
+import { userActions } from '@/redux/slice/userSlice';
 import { AppDispatch } from '@/redux/store';
-import { getAccessToken } from '@/service/auth';
+import { getAccessToken, getUser } from '@/service/auth';
+import { User } from '@/types/user';
 
 export async function storeAccessToken(dispatch: AppDispatch) {
   try {
     const accessToken = await getAccessToken();
     dispatch(authActions.setAccessToken(accessToken));
     localStorage.setItem(LOGIN, JSON.stringify(true));
+
+    return accessToken;
   } catch (error) {
     localStorage.removeItem(LOGIN);
-    return;
+  }
+}
+
+export async function storeUser(
+  accessToken: string,
+  dispatch: AppDispatch
+): Promise<User | undefined> {
+  try {
+    const user = await getUser(accessToken);
+    dispatch(userActions.setUser(user));
+
+    return user;
+  } catch (error) {
+    localStorage.removeItem(LOGIN);
   }
 }
