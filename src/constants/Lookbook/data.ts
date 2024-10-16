65c7dcd65685d1d7f69ff70dd5aa686f20bd60f6
presentKey <kim.hyeonjae4@gmail.com>
2024-10-16 21:27:36 +0900

    feat: 남자 상의 옷 리스트 상수 데이터

--- src/constants/Lookbook/data.ts
+++ src/constants/Lookbook/data.ts
@@ -1,4 +1,12 @@
 import { WeatherType } from '@/types/weather';
+import 반팔티 from '@/components/clothes/반팔티';
+import 민소매 from '@/components/clothes/민소매';
+import 긴팔티 from '@/components/clothes/긴팔티';
+import 후드티 from '@/components/clothes/후드티';
+import 니트 from '@/components/clothes/니트';
+import 트렌치코트 from '@/components/clothes/트렌치코트';
+import 코트 from '@/components/clothes/코트';
+import 패딩 from '@/components/clothes/패딩';
 
 export const LOOKBOOK_WEATHER_TYPE: Record<
   WeatherType,
@@ -37,3 +45,14 @@ export const LOOKBOOK_WEATHER_TYPE: Record<
     color: '#D4D0FD',
   },
 };
+
+export const MAN_TOP_COLTHES = [
+  { name: '민소매', Clothes: 민소매 },
+  { name: '반팔티', Clothes: 반팔티 },
+  { name: '긴팔티', Clothes: 긴팔티 },
+  { name: '후드티', Clothes: 후드티 },
+  { name: '니트', Clothes: 니트 },
+  { name: '트렌치코트', Clothes: 트렌치코트 },
+  { name: '코트', Clothes: 코트 },
+  { name: '패딩', Clothes: 패딩 },
+];
