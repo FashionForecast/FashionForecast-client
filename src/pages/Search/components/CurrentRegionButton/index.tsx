d4e30aea4cf527030c5c381bdc5f567cf20bdaa8
presentKey <kim.hyeonjae4@gmail.com>
2024-10-25 15:52:56 +0900

    feat: 현재 위치 설정 구현

--- src/pages/Search/components/CurrentRegionButton/index.tsx
+++ src/pages/Search/components/CurrentRegionButton/index.tsx
@@ -1,30 +1,79 @@
 import { S } from './style';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { MY_REGION } from '@/constants/localStorage/key';
 import useGeolocation from '@/hooks/useGeolocation';
 import CustomButton from '@/components/CustomMui/CustomButton';
 import LocationIcon from '@/components/icon/Location';
+import useAppSelector from '@/hooks/useAppSelector';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { setUserRegion } from '@/service/auth';
+import { useState } from 'react';
+import { useSnackbar } from '@/contexts/SnackbarProvider';
+import { storeUser } from '@/utils/auth';
+import useAppDispatch from '@/hooks/useAppDispatch';
+
+export type SearchLocationState = {
+  state?: {
+    mode?: 'set';
+  };
+};
 
 const CurrentRegionButton = () => {
+  const accessToken = useAppSelector((state) => state.auth.accessToken);
+  const [isLoading, setIsLoading] = useState(false);
+
   const { geolocation, updateGPSRegion } = useGeolocation();
+  const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  const { state }: SearchLocationState = useLocation();
+  const { openSnackbar } = useSnackbar();
+
+  const { mutate } = useMutation({
+    mutationFn: () => setUserRegion('DEFAULT', accessToken),
+  });
 
   const handleClick = () => {
+    if (state?.mode === 'set') {
+      updatePersonalRegionSetting();
+      return;
+    }
+
+    setCurrntRegionUpdate();
+  };
+
+  const setCurrntRegionUpdate = () => {
     updateGPSRegion();
     localStorage.removeItem(MY_REGION);
     navigate('/');
   };
 
+  const updatePersonalRegionSetting = () => {
+    setIsLoading(true);
+    mutate(undefined, {
+      onSuccess: async () => {
+        await queryClient.invalidateQueries({ queryKey: ['weather'] });
+        await storeUser(accessToken, dispatch);
+        navigate('/user');
+      },
+      onError: () => openSnackbar('위치 설정 변경에 오류가 발생했어요'),
+      onSettled: () => setIsLoading(false),
+    });
+  };
+
+  const isDisabled = () =>
+    isLoading || (state?.mode !== 'set' && geolocation?.isGPS);
+
   return (
     <S.Wrapper>
       <CustomButton
         variant='outlined'
-        disabled={geolocation?.isGPS}
+        disabled={isDisabled()}
         color='inherit'
         size='large'
         fullWidth
         startIcon={
-          <LocationIcon color={geolocation?.isGPS ? 'disabled' : 'default'} />
+          <LocationIcon color={isDisabled() ? 'disabled' : 'default'} />
         }
         onClick={handleClick}
       >
