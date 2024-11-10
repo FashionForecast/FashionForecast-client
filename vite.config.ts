1d4fa6fd2dfb49ec3985bf6f682c0ac324e9d9d5
presentKey <kim.hyeonjae4@gmail.com>
2024-11-11 01:10:05 +0900

    feat: prerender 제거

--- vite.config.ts
+++ vite.config.ts
@@ -2,31 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import svgr from 'vite-plugin-svgr';
-import prerender from '@prerenderer/rollup-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr(),
-    prerender({
-      routes: ['/', '/search', '/login', '/feedback'],
-      renderer: '@prerenderer/renderer-puppeteer',
-      server: {
-        port: 3000,
-        host: 'localhost',
-      },
-      rendererOptions: {
-        maxConcurrentRoutes: 1,
-        renderAfterTime: 500,
-      },
-      postProcess(renderedRoute) {
-        renderedRoute.html = renderedRoute.html
-          .replace(/http:/i, 'https:')
-          .replace(/(https:\/\/)?(localhost|127\.0\.0\.1):\d*/i, 'ootc.life');
-      },
-    }),
-  ],
+  plugins: [react(), svgr()],
   resolve: {
     alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
   },
