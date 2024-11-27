7ef399d49d7489bf0fbea6bb4dd70a4c6de6dc6c
presentKey <kim.hyeonjae4@gmail.com>
2024-11-28 00:48:47 +0900

    refactor: pcp, pop, temperature Icon lazy 적용

--- src/components/icon/temperature/TemperatureIcon.tsx
+++ src/components/icon/temperature/TemperatureIcon.tsx
@@ -1,8 +1,10 @@
-import ColdIcon from './ColdIcon';
-import CoolIcon from './CoolIcon';
-import HotIcon from './HotIcon';
-import ModerateIcon from './ModerateIcon';
-import WarmIcon from './WarmIcon';
+import IconLoading from '@/components/IconLoading/IconLoading';
+import { lazy, Suspense } from 'react';
+const ColdIcon = lazy(() => import('./ColdIcon'));
+const CoolIcon = lazy(() => import('./CoolIcon'));
+const HotIcon = lazy(() => import('./HotIcon'));
+const ModerateIcon = lazy(() => import('./ModerateIcon'));
+const WarmIcon = lazy(() => import('./WarmIcon'));
 
 type TemperatureIconProps = {
   extremumTmp: number;
@@ -12,11 +14,21 @@ type TemperatureIconProps = {
  * 기온에 따른 아이콘 표시
  */
 const TemperatureIcon = ({ extremumTmp }: TemperatureIconProps) => {
-  if (extremumTmp <= 16) return <ColdIcon />;
-  if (extremumTmp <= 19) return <CoolIcon />;
-  if (extremumTmp <= 22) return <ModerateIcon />;
-  if (extremumTmp <= 27) return <WarmIcon />;
-  return <HotIcon />;
+  const Icon = getIcon(extremumTmp);
+
+  return (
+    <Suspense fallback={<IconLoading $width={40} $height={40} />}>
+      <Icon />
+    </Suspense>
+  );
 };
 
 export default TemperatureIcon;
+
+function getIcon(extremumTmp: number) {
+  if (extremumTmp <= 16) return ColdIcon;
+  if (extremumTmp <= 19) return CoolIcon;
+  if (extremumTmp <= 22) return ModerateIcon;
+  if (extremumTmp <= 27) return WarmIcon;
+  return HotIcon;
+}
