b77481f9f94356e39b71233dfd9fcf26331e16ba
presentKey <kim.hyeonjae4@gmail.com>
2024-11-29 22:34:19 +0900

    feat: 유효하지 않는 비회원 uuid 처리 로직 변경

--- src/services/search.ts
+++ src/services/search.ts
@@ -14,12 +14,12 @@ export async function getRecentSearchList(
 
     return await fetchAPI<RecentSearchList>(`/search/${uuid}`);
   } catch (error) {
-    // FIXME: 서버 검증 로직 변경 시, 수정 필요
     // 유효하지 않은 비회원 uuid인 경우, 새로운 uuid 발급
-    if (error === 'S002' && !memberId && !accessToken) {
+    if (String(error).includes('S002')) {
       const data = await guestLogin();
       if (data.uuid) {
         localStorage.setItem(GUEST_UUID, data.uuid);
+        return;
       }
     }
     throw new Error(error as string);
@@ -31,16 +31,28 @@ export async function registerResentSearch(
   memberId?: string,
   accessToken?: string | null
 ) {
-  const uuid =
-    memberId && accessToken ? memberId : localStorage.getItem(GUEST_UUID);
-  const [city, district1, district2] = region.split(' ');
-  const district = district2 ? `${district1} ${district2}` : district1;
+  try {
+    const uuid =
+      memberId && accessToken ? memberId : localStorage.getItem(GUEST_UUID);
+    const [city, district1, district2] = region.split(' ');
+    const district = district2 ? `${district1} ${district2}` : district1;
 
-  await fetchAPI(`/search/${uuid}`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ city, district }),
-  });
+    await fetchAPI(`/search/${uuid}`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ city, district }),
+    });
+  } catch (error) {
+    // 유효하지 않은 비회원 uuid인 경우, 새로운 uuid 발급
+    if (String(error).includes('S002')) {
+      const data = await guestLogin();
+      if (data.uuid) {
+        localStorage.setItem(GUEST_UUID, data.uuid);
+        return;
+      }
+    }
+    throw new Error(error as string);
+  }
 }
 
 export async function deleteSearchWord(
