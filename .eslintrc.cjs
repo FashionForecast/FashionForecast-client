57a43ee72a62c6d2c4b86c2b9ac4444a9c1b56d7
presentKey <kim.hyeonjae4@gmail.com>
2025-02-16 01:43:31 +0900

    chore: eslint import 순서 규칙 적용

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,48 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'region.cjs', 'robots.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'import'],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.app.json',
+      },
+    },
+  },
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+    ],
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        pathGroups: [
+          { pattern: '@/app/**', group: 'internal', position: 'before' },
+          { pattern: '@/pages/**', group: 'internal', position: 'before' },
+          { pattern: '@/widgets/**', group: 'internal', position: 'before' },
+          { pattern: '@/features/**', group: 'internal', position: 'before' },
+          { pattern: '@/entities/**', group: 'internal', position: 'before' },
+          { pattern: '@/shared/**', group: 'internal', position: 'before' },
+        ],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
     ],
   },
 };
