2e3b6dec9c41386228d2e29f44f8a075acdda6d0
presentKey <kim.hyeonjae4@gmail.com>
2025-02-16 23:07:22 +0900

    design: theme 색상 추가 및 변경

--- src/shared/styles/theme.ts
+++ src/shared/styles/theme.ts
@@ -4,25 +4,26 @@ import { typo } from './typo';
 export const theme = {
   colors: {
     text: {
-      primary: colors.blueGrey['A87'],
+      primary: colors.blueGrey[900],
       secondary: colors.blueGrey['A60'],
       disabled: colors.blueGrey['A38'],
-    },
-    info: {
-      main: colors.blue['600'],
+      hover: colors.blueGrey['A04'],
+      selected: colors.blueGrey['A08'],
+      focus: colors.blueGrey.A12,
+      focusVisible: colors.blueGrey.A30,
+      outlinedBorder: colors.blueGrey.A50,
     },
     primary: {
-      main: colors.blueGrey['600'],
-      dark: colors.blueGrey['700'],
+      main: colors.blueGrey[600],
+      dark: colors.blueGrey[700],
+      light: colors.blueGrey[300],
     },
     primaryState: {
       hover: colors.blueGrey['A04'],
-    },
-    elevation: {
-      outlined: colors.blueGrey['300'],
-    },
-    error: {
-      main: colors.red[600],
+      selected: colors.blueGrey.A08,
+      focus: colors.blueGrey.A12,
+      focusVisible: colors.blueGrey.A30,
+      outlineBorder: colors.blueGrey.A50,
     },
     action: {
       active: colors.blueGrey['A56'],
@@ -31,6 +32,40 @@ export const theme = {
       focus: colors.blueGrey['A12'],
       disabled: colors.blueGrey['A38'],
       disabledBackground: colors.blueGrey['A12'],
+    },
+    error: {
+      main: colors.red[600],
+      dark: colors.red[700],
+      light: colors.red[300],
+      hover: colors.red.A04,
+      selected: colors.red.A08,
+      focusVisible: colors.red.A30,
+      outlinedBorder: colors.red.A50,
+    },
+    warning: {
+      main: colors.orange[600],
+      dark: colors.orange[700],
+      light: colors.orange[300],
+      contrast: colors.white,
+    },
+    info: {
+      main: colors.blue['600'],
+      dark: colors.blue[700],
+      light: colors.blue[300],
+      contrast: colors.white,
+    },
+    success: {
+      main: colors.green[600],
+      dark: colors.green[700],
+      light: colors.green[300],
+      contrast: colors.white,
+    },
+    secondary: {
+      main: colors.blueGrey[200],
+      dark: colors.blueGrey[900],
+    },
+    elevation: {
+      outlined: colors.blueGrey['300'],
     },
     blueGrey: colors.blueGrey,
     red: colors.red,
