c254f9e250c3a54d5a124c3214bfc7a3be1337a4
presentKey <kim.hyeonjae4@gmail.com>
2025-02-16 23:22:42 +0900

    design: typo 변경

--- src/shared/styles/typo.ts
+++ src/shared/styles/typo.ts
@@ -1,20 +1,46 @@
 import { css } from '@emotion/react';
 
 export const typo = {
-  h5: getTypoStyle(24, 32.02, 0),
-  h6: getTypoStyle(20, 24, 0.15, 500),
-  'body-1': getTypoStyle(16, 24, 0.15),
-  'body-2': getTypoStyle(14, 20.02, 0.17),
-  'subtitle-1': getTypoStyle(16, 28, 0.15, 700),
-  'subtitle-2': getTypoStyle(14, 21.98, 0.1, 500),
+  h5: setTypoStyle({ fontSize: 24, lineHeight: 32.02, letterSpacing: 0 }),
+  h6: setTypoStyle({
+    fontSize: 20,
+    lineHeight: 24,
+    letterSpacing: 0.15,
+    fontWeight: 500,
+  }),
+  'body-1': setTypoStyle({ fontSize: 16, lineHeight: 24, letterSpacing: 0.15 }),
+  'body-2': setTypoStyle({
+    fontSize: 14,
+    lineHeight: 20.02,
+    letterSpacing: 0.17,
+  }),
+  'subtitle-1': setTypoStyle({
+    fontSize: 16,
+    lineHeight: 28,
+    letterSpacing: 0.15,
+    fontWeight: 700,
+  }),
+  'subtitle-2': setTypoStyle({
+    fontSize: 14,
+    lineHeight: 21.98,
+    letterSpacing: 0.1,
+    fontWeight: 700,
+  }),
 } as const;
 
-function getTypoStyle(
-  fontSize: number,
-  lineHeight: number,
-  letterSpacing: number,
-  fontWeight: number = 400
-) {
+type TypoStyle = {
+  fontSize: number;
+  lineHeight: number;
+  letterSpacing: number;
+  fontWeight?: number;
+};
+
+function setTypoStyle({
+  fontSize,
+  lineHeight,
+  letterSpacing,
+  fontWeight = 400,
+}: TypoStyle) {
   return css`
     font-size: ${fontSize}px;
     font-weight: ${fontWeight};
