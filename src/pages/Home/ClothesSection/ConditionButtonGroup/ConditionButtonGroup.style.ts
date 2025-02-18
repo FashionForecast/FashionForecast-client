ebdad5147f0bcc86d79b1a285fca938709b66505
presentKey <kim.hyeonjae4@gmail.com>
2025-02-18 22:31:27 +0900

    design: ToogleButtonGroup 적용

--- src/pages/Home/ClothesSection/ConditionButtonGroup/ConditionButtonGroup.style.ts
+++ src/pages/Home/ClothesSection/ConditionButtonGroup/ConditionButtonGroup.style.ts
@@ -1,33 +1,8 @@
 import styled from '@emotion/styled';
 
-import { CustomToggleButton } from '@/shared/ui';
-
-const ToggleButton = styled(CustomToggleButton)`
-  height: 40px;
-
-  &.MuiToggleButtonGroup-middleButton {
-    border-right: 1px solid ${({ theme }) => theme.colors.primary.main};
-    border-left: 1px solid ${({ theme }) => theme.colors.primary.main};
-  }
-
-  &:first-of-type {
-    border-top-left-radius: 16px;
-    border-bottom-left-radius: 16px;
-  }
-
-  &:last-of-type {
-    border-top-right-radius: 16px;
-    border-bottom-right-radius: 16px;
-  }
-`;
-
 const ButtonWrap = styled.div`
   padding: 0 16px;
 `;
-
-export const C = {
-  ToggleButton,
-};
 
 export const S = {
   ButtonWrap,
