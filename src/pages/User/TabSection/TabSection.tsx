be8538316889ae52aeb806379cc71eabb30cda60
presentKey <kim.hyeonjae4@gmail.com>
2025-02-24 23:50:45 +0900

    feat: 기존 CustomTabs 제거 및 Tabs 컴포넌트로 대체

--- src/pages/User/TabSection/TabSection.tsx
+++ src/pages/User/TabSection/TabSection.tsx
@@ -1,23 +1,18 @@
 import { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
-import {
-  BookFillIcon,
-  BookOutlineIcon,
-  UserOutlineIcon,
-  UserFillIcon,
-} from '@/shared/ui';
+import { Tabs } from '@/shared/ui';
 
 import LookbookList from './LookbookList/LookbookList';
 import SettingList from './SettingList/SettingList';
-import { C, S } from './TabSection.style';
+import { S } from './TabSection.style';
 
-type TabState = '룩북' | '내 설정';
+type TabState = '룩북' | '설정';
 
 const TabSection = () => {
   const [searchParams] = useSearchParams();
   const [tab, setTab] = useState<TabState>(
-    searchParams.get('tab') === 'set' ? '내 설정' : '룩북'
+    searchParams.get('tab') === 'set' ? '설정' : '룩북'
   );
 
   const handleTabChange = (_: React.SyntheticEvent, tab: TabState) => {
@@ -26,24 +21,11 @@ const TabSection = () => {
 
   return (
     <S.SectionWrap>
-      <C.Tabs value={tab} onChange={handleTabChange} variant='fullWidth'>
-        <C.Tab
-          value='룩북'
-          label='룩북'
-          icon={tab === '룩북' ? <BookFillIcon /> : <BookOutlineIcon />}
-          iconPosition='start'
-        />
-        <C.Tab
-          value='내 설정'
-          label='내 설정'
-          icon={tab === '내 설정' ? <UserFillIcon /> : <UserOutlineIcon />}
-          iconPosition='start'
-        />
-      </C.Tabs>
+      <Tabs labels={['룩북', '설정']} value={tab} onChange={handleTabChange} />
 
       <S.ContentWrap>
         {tab === '룩북' && <LookbookList />}
-        {tab === '내 설정' && <SettingList />}
+        {tab === '설정' && <SettingList />}
       </S.ContentWrap>
     </S.SectionWrap>
   );
