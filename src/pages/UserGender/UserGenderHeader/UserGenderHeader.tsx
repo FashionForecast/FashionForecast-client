77c5253602e74b0954658486898a300e4739d751
presentKey <kim.hyeonjae4@gmail.com>
2025-02-26 03:19:04 +0900

    fix: type 오류 수정

--- src/pages/UserGender/UserGenderHeader/UserGenderHeader.tsx
+++ src/pages/UserGender/UserGenderHeader/UserGenderHeader.tsx
@@ -1,11 +1,8 @@
 import { IconButton } from '@mui/material';
 
-import { useAppSelector } from '@/shared/lib/useAppSelector';
 import { Header, UserAvatar } from '@/shared/ui';
 
 const UserGenderHeader = () => {
-  const user = useAppSelector((state) => state.member.info);
-
   return (
     <Header
       leftSlot={
@@ -13,7 +10,7 @@ const UserGenderHeader = () => {
           <img src='/logo.svg' alt='로고 이미지' />
         </IconButton>
       }
-      rightSlot={<UserAvatar imageUrl={user?.imageUrl} />}
+      rightSlot={<UserAvatar />}
     />
   );
 };
