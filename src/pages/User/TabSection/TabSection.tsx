d61758c3ea5eb493fb0eff260ae885a9a5ed752e
presentKey <kim.hyeonjae4@gmail.com>
2025-03-01 01:18:02 +0900

    feat: 사용자 페이지 Tabs 컴포넌트의 items 속성 변경

--- src/pages/User/TabSection/TabSection.tsx
+++ src/pages/User/TabSection/TabSection.tsx
@@ -7,25 +7,29 @@ import LookbookList from './LookbookList/LookbookList';
 import SettingList from './SettingList/SettingList';
 import { S } from './TabSection.style';
 
-type TabState = '룩북' | '설정';
+type MemberTab = 'lookbook' | 'set';
+const MEMBER_TAB = [
+  { title: '룩북', value: 'lookbook' },
+  { title: '설정', value: 'set' },
+];
 
 const TabSection = () => {
   const [searchParams] = useSearchParams();
-  const [tab, setTab] = useState<TabState>(
-    searchParams.get('tab') === 'set' ? '설정' : '룩북'
+  const [tab, setTab] = useState<MemberTab>(
+    searchParams.get('tab') === 'set' ? 'set' : 'lookbook'
   );
 
-  const handleTabChange = (_: React.SyntheticEvent, tab: TabState) => {
+  const handleTabChange = (_: React.SyntheticEvent, tab: MemberTab) => {
     setTab(tab);
   };
 
   return (
     <S.SectionWrap>
-      <Tabs labels={['룩북', '설정']} value={tab} onChange={handleTabChange} />
+      <Tabs items={MEMBER_TAB} value={tab} onChange={handleTabChange} />
 
       <S.ContentWrap>
-        {tab === '룩북' && <LookbookList />}
-        {tab === '설정' && <SettingList />}
+        {tab === 'lookbook' && <LookbookList />}
+        {tab === 'set' && <SettingList />}
       </S.ContentWrap>
     </S.SectionWrap>
   );
