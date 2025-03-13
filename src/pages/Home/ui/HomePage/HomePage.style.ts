8abcbb8500c6742bdf2aff61a56fa33898976a1c
presentKey <kim.hyeonjae4@gmail.com>
2025-03-13 16:17:49 +0900

    style: FashionContent 및 HomePage 스타일 수정

--- src/pages/Home/ui/HomePage/HomePage.style.ts
+++ src/pages/Home/ui/HomePage/HomePage.style.ts
@@ -4,11 +4,12 @@ const HomeWrap = styled.div`
   display: flex;
   flex-direction: column;
   min-height: 100dvh;
+  padding-bottom: 114px;
+  background-color: ${({ theme }) => theme.colors.blueGrey[100]};
 `;
 
 const TabsWrap = styled.div`
   padding-left: 10px;
-  background-color: ${({ theme }) => theme.colors.blueGrey[100]};
 `;
 
 export const S = { HomeWrap, TabsWrap };
