41188be5902bb7d4a697bfdec00e086090e2f031
presentKey <kim.hyeonjae4@gmail.com>
2025-03-21 01:02:54 +0900

    feat: LookbookClothes 컴포넌트 구현

--- src/entities/clothes/index.ts
+++ src/entities/clothes/index.ts
@@ -5,3 +5,6 @@ export {
 } from './api/lookbook';
 
 export type { ClothesIconNames, ClothesType } from './model/types';
+
+export { ClothesIcon } from './ui/ClothesIcon/ClothesIcon';
+export { LookbookClothes } from './ui/LookbookClothes/LookbookClothes';
---
+++ src/entities/clothes/ui/LookbookClothes/LookbookClothes.style.ts
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+const TopClothes = styled.div`
+  position: relative;
+  top: 16px;
+`;
+
+export const S = { TopClothes };
