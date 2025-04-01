4d58dd5526e26f888ad296416a90c449ad132cb6
presentKey <kim.hyeonjae4@gmail.com>
2025-04-01 20:22:22 +0900

    feat: 룩북 옷의 상, 하의 간격 조정

--- src/entities/clothes/ui/LookbookClothes/LookbookClothes.style.ts
+++ src/entities/clothes/ui/LookbookClothes/LookbookClothes.style.ts
@@ -1,8 +1,20 @@
 import styled from '@emotion/styled';
 
-const TopClothes = styled.div`
+const ClothesWrap = styled.div<{ $top: number }>`
   position: relative;
-  top: 16px;
+  top: ${({ $top }) => `${-$top}px`};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
-export const S = { TopClothes };
+const TopClothes = styled.div<{ $top: number }>`
+  position: relative;
+  top: ${({ $top }) => `${$top}px`};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const S = { TopClothes, ClothesWrap };
