6df5396ace2fd39b9fbd498417ed76348d3e55d6
presentKey <kim.hyeonjae4@gmail.com>
2025-04-01 20:55:32 +0900

    design: 터치 피드백 효과 색상을 투명으로 변경

--- src/widgets/time/ui/TimeBottomSheet/TimeBottomSheet.style.ts
+++ src/widgets/time/ui/TimeBottomSheet/TimeBottomSheet.style.ts
@@ -16,6 +16,7 @@ const BottomSheet = styled.div`
   border-top-right-radius: ${({ theme }) => theme.borderRadius[2]};
   box-shadow: 0 5px 5px -3px rgb(0 0 0 / 20%), 0 8px 10px 1px rgb(0 0 0 / 14%),
     0 3px 14px 2px rgb(0 0 0 / 12%);
+  -webkit-tap-highlight-color: transparent;
 `;
 
 const RangesWrap = styled.div<{ $isCompactRanges: boolean }>`
